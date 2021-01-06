c0cb1a9cc2cadc7c1f8fff8a370fbbb204ed5354
khoaHyh <khoahuynh.toronto@gmail.com>
2021-01-05 21:25:49 -0500

    Feat: Add handleClick for manipulate-time
    The length will display the times respective to how many clicks the
    increment and decrement buttons receive.

--- src/components/Manipulate.js
+++ src/components/Manipulate.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const ManipulateTime = styled.div`
@@ -9,14 +10,31 @@ const ManipulateTime = styled.div`
     align-items: center;
 `
 
+let initialValue = 5;
+let re = /decrement/i;
+
 const Manipulate = ({ label, decrement, length, increment, text }) => {
-    
+    if (label === 'session-label') {
+        initialValue = 25;
+    }
+    const [display, setDisplay] = useState(initialValue);
+
+
+    const handleClick = (event) => {
+        console.log(event.target.id);
+        if (re.test(event.target.id) && display > 0) {
+            setDisplay(display - 1);
+        } else {
+            setDisplay(display + 1);
+        }
+    }
+
     return (
         <ManipulateTime className="manipulate-time">
             <div id={label}>{text}</div>
-            <button id={decrement}>-</button>
-            <div id={length}>placeholder</div>
-            <button id={increment}>+</button>
+            <button id={decrement} onClick={handleClick}>-</button>
+            <div id={length}>{display}</div>
+            <button id={increment} onClick={handleClick}>+</button>
         </ManipulateTime>
     );
 }
