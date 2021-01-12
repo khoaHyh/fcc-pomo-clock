ecb78ebdafd33d7d52aa9884ec1a791f0772af6e
khoaHyh <khoahuynh.toronto@gmail.com>
2021-01-12 16:53:22 -0500

    Feat: Abstract Timer into own component
    Timer will now be a separate component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import './App.css';
 import ReactFCCtest from 'react-fcctest';
 import ManList from './components/ManList';
+import Timer from './components/Timer';
 import styled from 'styled-components';
+
 
 const manipulateTime = [
   {
@@ -18,37 +21,29 @@ const manipulateTime = [
     increment: 'session-increment',
     text: 'Session Length'
   }
-];
-
-const TimerContainer = styled.div`
-    margin: 0.75rem;
-    padding: 0.5rem;
-    width: 8rem;
-    background-color: salmon;
-    color: white;
-    border: solid 1px black;
-    border-radius: 0.25rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+];const TimerContainer = styled.div`
+margin: 0.75rem;
+padding: 0.5rem;
+width: 8rem;
+background-color: salmon;
+color: white;
+border: solid 1px black;
+border-radius: 0.25rem;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
 `
 
+
 const App = () => {
+  const [tLabel, setTlabel] = useState('Session');
 
   return (
     <div className='App'>
       <ReactFCCtest />
       <ManList manipulateTime={manipulateTime} /> 
-      <TimerContainer className="timer">
-        <div id="timer-label">Session</div>
-        <button id="start_stop">start/stop</button>
-        <div id="time-left">
-          <span></span>:
-          <span></span>
-        </div>
-        <button id="reset">reset</button>
-      </TimerContainer>
+      <Timer timerLabel={tLabel} />  
     </div>
   );
 }
